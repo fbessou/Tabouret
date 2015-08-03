46732b42d0dd126c67ff21c34489e70a42bedb67
Pro1d <topaffaire_pg@hotmail.fr>
2015-08-03 20:08:01 +0200

    Rename a member of GamePadIOClient

--- Sofa/src/com/fbessou/sofa/GamePadIOHelper.java
+++ Sofa/src/com/fbessou/sofa/GamePadIOHelper.java
@@ -5,8 +5,8 @@ import android.app.Activity;
 import com.fbessou.sofa.GamePadIOClient.GameMessageListener;
 import com.fbessou.sofa.sensor.Sensor;
 
-public class GamePadIOHelper implements GameMessageListener {
-	GamePadIOClient mGameBinder;
+public class GamePadIOHelper {
+	GamePadIOClient mGamePadIO;
 	// TODO ArrayList<Indicator>
 	
 	public GamePadIOHelper() {
@@ -14,35 +14,36 @@ public class GamePadIOHelper implements GameMessageListener {
 	}
 	
 	public void start(Activity activity, GamePadInformation info) {
-		mGameBinder = GamePadIOClient.getGamePadIOClient(activity, info);
-		mGameBinder.setGameMessageListener(this);
+		mGamePadIO = GamePadIOClient.getGamePadIOClient(activity, info);
+		mGamePadIO.setGameMessageListener(new GameMessage());
 	}
 	
 	public void attachSensor(Sensor sensor) {
-		if(mGameBinder != null)
-			mGameBinder.addSensor(sensor);
+		if(mGamePadIO != null)
+			mGamePadIO.addSensor(sensor);
 		else
 			throw new RuntimeException("To attach sensor, the method start(activity, info) must be invoked first");
 	}
 	
 	public void updateInformation(GamePadInformation info) {
-		mGameBinder.updateGamePadInfo(info);
+		mGamePadIO.updateGamePadInfo(info);
 	}
 	
-	@Override
-	public void onGameOutputReceived(OutputEvent event) {
-		// TODO Auto-generated method stub
-		
+	private class GameMessage implements GameMessageListener {
+		@Override
+		public void onGameOutputReceived(OutputEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+	
+		@Override
+		public void onGameRenamed(String newName) {
+			// TODO Auto-generated method stub
+		}
+	
+		@Override
+		public void onGameLeft() {
+			// TODO Auto-generated method stub
+		}
 	}
-
-	@Override
-	public void onGameRenamed(String newName) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void onGameLeft() {
-		// TODO Auto-generated method stub
-	}
-
 }
