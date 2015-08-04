85bdfd81de4286c66034f53ef6ddffc8e11a03d5
Pro1d <topaffaire_pg@hotmail.fr>
2015-08-04 21:38:18 +0200

    Add abstract Indicator, boolean indicator and text indicator

---
+++ Sofa/src/com/fbessou/sofa/indicator/BooleanIndicator.java
@@ -0,0 +1,39 @@
+package com.fbessou.sofa.indicator;
+
+import android.widget.CompoundButton;
+
+import com.fbessou.sofa.OutputEvent;
+
+public class BooleanIndicator extends Indicator {
+	
+	/** Last string received **/
+	private boolean mIsOn;
+	private CompoundButton mAttachedView;
+	
+	public BooleanIndicator() {
+		super();
+	}
+	public BooleanIndicator(int id) {
+		super(id);
+	}
+	
+	/** Attaches this indicator to a compound button (ie: CheckBox, Switch,
+	 * ToggleButton, and others two state view). Each output event received
+	 * will be display in this view. **/
+	public void attachTo(CompoundButton view) {
+		mAttachedView = view;
+	}
+	
+	@Override
+	public void onOututEventReceived(OutputEvent event) {
+		mIsOn = (event.state != 0);
+		
+		/** Update the view on the UI thread **/
+		mUIHandler.post(new Runnable() {
+			@Override
+			public void run() {
+				mAttachedView.setChecked(mIsOn);	
+			}
+		});
+	}
+}
